068958528b73f9cb992d34779badc62065fc338b
Tech-RVRG <technicalsupport@rvrg.in>
2026-06-11 17:23:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("RVRG Store Management Loaded");
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+async function loadMaterials() {
+
+    const { data, error } = await supabaseClient
+        .from('materials')
+        .select('*');
+
+    if (error) {
+        console.error(error);
+        alert("Error loading materials");
+        return;
+    }
+
+    const tbody =
+        document.querySelector("#materialsTable tbody");
+
+    tbody.innerHTML = "";
+
+    data.forEach(material => {
+
+        tbody.innerHTML += `
+            <tr>
+                <td>${material.material_code}</td>
+                <td>${material.material_name}</td>
+                <td>${material.unit}</td>
+            </tr>
+        `;
+
+    });
+}
