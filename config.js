077975cef695c42cd5d96ad5f37b666187eaa86e
Tech-RVRG <technicalsupport@rvrg.in>
2026-06-11 17:22:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://immkxmskfeoksebnlidv.supabase.co";
 
-const SUPABASE_ANON_KEY = "PASTE_YOUR_PUBLIC_KEY_HERE";
+const SUPABASE_ANON_KEY = "sb_publishable_mVD5kA-c8Lzo2Md9yl4g7w_6V7TXaJO";
