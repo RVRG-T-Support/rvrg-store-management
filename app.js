6b88517089c302b206cc17804e03e00bdfe6863e
Tech-RVRG <technicalsupport@rvrg.in>
2026-06-11 18:06:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,96 @@ const supabaseClient = supabase.createClient(
     SUPABASE_ANON_KEY
 );
 
-async function loadMaterials() {
+loadDepartments();
+loadMaterials();
 
-    const { data, error } = await supabaseClient
-        .from("materials")
-        .select("*");
+async function loadDepartments() {
+
+    const { data, error } =
+        await supabaseClient
+        .from("departments")
+        .select("*")
+        .order("department_name");
 
     if (error) {
         console.error(error);
-        alert("Error loading materials");
+        return;
+    }
+
+    const select =
+        document.getElementById("departmentId");
+
+    select.innerHTML =
+        '<option value="">Select Department</option>';
+
+    data.forEach(dep => {
+
+        select.innerHTML += `
+            <option value="${dep.id}">
+                ${dep.department_name}
+            </option>
+        `;
+
+    });
+
+}
+
+async function addMaterial() {
+
+    const materialCode =
+        document.getElementById("materialCode").value;
+
+    const materialName =
+        document.getElementById("materialName").value;
+
+    const departmentId =
+        document.getElementById("departmentId").value;
+
+    const unit =
+        document.getElementById("unit").value;
+
+    const minimumStock =
+        document.getElementById("minimumStock").value;
+
+    const { error } =
+        await supabaseClient
+        .from("materials")
+        .insert([
+            {
+                material_code: materialCode,
+                material_name: materialName,
+                department_id: departmentId,
+                unit: unit,
+                minimum_stock: minimumStock
+            }
+        ]);
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+    alert("Material Saved");
+
+    loadMaterials();
+
+}
+
+async function loadMaterials() {
+
+    const { data, error } =
+        await supabaseClient
+        .from("materials")
+        .select(`
+            *,
+            departments (
+                department_name
+            )
+        `)
+        .order("material_code");
+
+    if (error) {
+        console.error(error);
         return;
     }
 
@@ -26,7 +107,9 @@ async function loadMaterials() {
         <tr>
             <td>${material.material_code}</td>
             <td>${material.material_name}</td>
+            <td>${material.departments.department_name}</td>
             <td>${material.unit}</td>
+            <td>${material.minimum_stock}</td>
         </tr>
         `;
 
