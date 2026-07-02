1e295f2e92407104c888361d9bfedfb9c33127d2
Tech-RVRG <technicalsupport@rvrg.in>
2026-07-02 14:51:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,19 @@
 const SUPABASE_URL = "https://immkxmskfeoksebnlidv.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_mVD5kA-c8Lzo2Md9yl4g7w_6V7TXaJO";
+
+// ===== USER ROLE =====
+
+const CURRENT_USER = {
+
+    name: "Store Keeper",
+
+    role: "STORE"
+
+};
+
+// Roles:
+// STORE
+// AFM
+// FM
+// ADMIN
