a950c0b87dc8e086e43e5395b5e851b0c9ba522a
Tech-RVRG <technicalsupport@rvrg.in>
2026-07-13 10:41:09 +0530

    Update and rename add_material.js to add_material v2.js

--- add_material v2.js
+++ add_material v2.js
@@ -165,3 +165,22 @@ async function generateMaterialCode() {
             .padStart(3, "0");
 
 }
+/*
+
+Reference Map
+
+MAT-02A  Initialization
+
+MAT-02B  Load Departments
+
+MAT-02C  GST Change
+
+MAT-03   Auto Material Code
+
+MAT-04   Validation
+
+MAT-05   Save Material
+
+MAT-06   Reset Form
+
+*/
